6e8ea8a7267bbc52c6a6e6a3b35b08948acfad46
Damián Čopík <dcopik25@gmail.com>
2020-12-19 15:28:21 +0100

    Added API for displaying content of db tables

--- api/app.py
+++ api/app.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, instance_relative_config=True)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../database.db" 
 
 try:
   os.makedirs(app.instance_path)
@@ -9,12 +11,20 @@ try:
 except OSError:
   pass
 
+db = SQLAlchemy(app)
 app.config.from_pyfile("settings.py")
 
 
-@app.route("/api")
-def index():
-  return jsonify()
+@app.route("/api/<string:apiType>")
+def patients(apiType):
+  print(app.config["TEST"])
+  if(not db.engine.dialect.has_table(db.engine, apiType)):
+    abort(404)
+  dbRes = db.session.execute("SELECT * FROM '" + apiType + "'");
+  result = []
+  for row in dbRes:
+    result.append(dict(row.items()))
+  return jsonify(result)
 
 if(__name__ == "__main__"):
   app.run(debug=True)
