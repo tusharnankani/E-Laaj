09103d78c86c7b306ff7c2fdd84b275506c612a0
Damián Čopík <dcopik25@gmail.com>
2020-12-20 18:22:48 +0100

    feat: Add create otp api

--- elaaj/views/api/__init__.py
+++ elaaj/views/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, abort, request
-from elaaj.models.db import db, PhoneCodes
+from elaaj.models.db import db, PhoneCodes, Patients, Doctors
 from datetime import datetime, timedelta
+from random import randint
 
 bp = Blueprint("api", __name__, url_prefix="/api")
 
@@ -30,6 +31,19 @@ def otpCheck():
   if(phoneDb and phoneDb.otp == otp and datetime.fromtimestamp(phoneDb.expire) > datetime.now()):
     return jsonify(isCorrect=True)
   return jsonify(isCorrect=False)
+
+@bp.route("/createOtp", methods=["POST"])
+def createOtp():
+  phone = request.json.get("phone")
+  if(not phone):
+    return jsonify(error="Provide a phone number!")
+  otp = int("".join([str(randint(0, 9)) for _ in range(0, 4)]))
+  if(Patients.query.filter_by(phone=phone) is not None or Doctors.query.filter_by(phone=phone) is not None):
+    return jsonify(error="This phone number is already registerd!")
+  db.session.add(PhoneCodes(phone=phone, otp=otp, expire=datetime.timestamp(datetime.now()+timedelta(minutes=3))))
+  db.session.commit()
+  db.session.remove()
+  return jsonify(otp=otp)
     
 @bp.route("/<string:apiType>")
 def apiDbGetter(apiType):
