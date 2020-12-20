9e0bd128b4db014a7223302ab5a5fff0060f3751
Damián Čopík <dcopik25@gmail.com>
2020-12-20 17:33:53 +0100

    feat: Phone otp check api

--- elaaj/views/api/__init__.py
+++ elaaj/views/api/__init__.py
@@ -1,7 +1,25 @@
-from flask import Blueprint, jsonify, abort
-from elaaj.models.db import db
+from flask import Blueprint, jsonify, abort, request
+from elaaj.models.db import db, PhoneCodes
+from datetime import datetime, timedelta
 
 bp = Blueprint("api", __name__, url_prefix="/api")
+
+@bp.route("/phoneCheck")
+def phoneCheck():
+  phone = request.args.get("phone")
+  otp = request.args.get("otp")
+  try:
+    phone=int(phone)
+  except:
+    return jsonify(isCorrect=False)
+
+  if(not phone or not otp):
+    return jsonify(isCorrect=False)
+  phoneDb = PhoneCodes.query.filter_by(phone=phone).first()
+  if(phoneDb and phoneDb.otp == otp and datetime.fromtimestamp(phoneDb.expire) > datetime.now() """not expired"""):
+    return jsonify(isCorrect=True)
+  return jsonify(isCorrect=False)
+    
 
 @bp.route("/<string:apiType>")
 def apiDbGetter(apiType):
